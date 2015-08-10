c87252b675a7c63933ddb1411073892e6c0dd559
Christoph Pirringer & Lisa Duschek <student@epicodus.com>
2015-08-10 15:37:12 -0700

    add the methods myStore and myFeetch to add &respectively read out a key/value pair. Pass the respective spec

--- lib/do_it_yourself.rb
+++ lib/do_it_yourself.rb
@@ -1,9 +1,22 @@
 require ('pry')
 
 class MyHash
-  define_method(:initialize) do |key, value|
-    @key=key
-    @value=value
+  define_method(:initialize) do
+    @hash=[]
+  end
+
+  define_method(:myStore) do |key, value|
+    @hash.push([key, value])
+  end
+
+  define_method(:myFetch) do |key|
+    value = nil
+    @hash.each() do |pair|
+      if pair[0] == key
+        value = pair[1]
+      end
+    end
+    value
   end
 
 end
